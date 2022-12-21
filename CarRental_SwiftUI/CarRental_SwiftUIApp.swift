42c7325315f4f5b46c9e015e05af01dfd4f36bf5
dabdelwahed <dabdelwahed@mobelite.fr>
2022-12-21 11:45:21 +0100

    Initial Commit

---
+++ CarRental_SwiftUI/CarRental_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarRental_SwiftUIApp.swift
+//  CarRental_SwiftUI
+//
+//  Created by Dorra Ben Abdelwahed on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarRental_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
