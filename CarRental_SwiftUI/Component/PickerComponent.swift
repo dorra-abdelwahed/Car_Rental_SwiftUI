590373928a7d3705f1d262e0d6d50d196d5963e7
dabdelwahed <dabdelwahed@mobelite.fr>
2022-12-27 14:19:57 +0100

    first commit

---
+++ CarRental_SwiftUI/Component/PickerComponent.swift
@@ -0,0 +1,20 @@
+//
+//  PickerComponent.swift
+//  CarRental_SwiftUI
+//
+//  Created by Dorra Ben Abdelwahed on 22/12/2022.
+//
+
+import SwiftUI
+
+struct PickerComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickerComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerComponent()
+    }
+}
